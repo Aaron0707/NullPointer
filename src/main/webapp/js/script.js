431b4697e8aa098445e29f3f389924b5906b6172
annezhao <K02200059>
2016-11-21 22:08:12 -0600

    HomePage refined

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,24 @@
 /**
  * Created by annezhao on 11/20/16.
  */
+
+$(document).ready(function() {
+    var screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    $("#index_bottom_anchor").css("margin-left", (screenWidth / 2 - 29).toString() + "px");
+
+    $(window).scroll(function() {
+        var index_Nav = $("#index_Nav");
+        var highlight = $("#navbar-collapse-01 :first-child :first-child");
+
+        if ($(window).scrollTop() >= screenHeight / 2) {
+            index_Nav.removeClass("transparent");
+            highlight.addClass("active");
+        } else {
+            index_Nav.addClass("transparent");
+            highlight.removeClass("active");
+        }
+    });
+});
+
